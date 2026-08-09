5626dea8caf3f132ecb6f4286a4bf47f64a3c04e
Smith19-dev <owardquintero2003@gmail.com>
2026-08-09 14:10:46 -0500

    validacion nde datos en register y login y creacion del front

---
+++ src/middleware/validator.middleware.js
@@ -0,0 +1,14 @@
+
+export const validateSchema = (schema) => (req, res, next) => {
+    try {
+        schema.parse(req.body);
+        next();
+
+    } catch (error) {
+        console.log("Error de Zod:", error);
+
+        return res.status(400).json({
+            error: error.issues.map((issue) => issue.message)
+        });
+    }
+};
--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -1,9 +1,13 @@
 import {z} from "zod"
 
 export const registerSchema = z.object({
-    username: z.string({required_error: 'Username is required'}),
-    name:z.string({required_error: 'Name is required'}),
-    lastName, 
-    phone,
-    password
+    username: z.string({message: 'Username is required'}),
+    name:z.string({message: 'Name is required'}),
+    lastName:z.string({message: 'Last name is required'}), 
+    password: z.string({message: 'Password is required'}).min(8,{message: 'Password must be at least 8 characters'})
+})
+
+export const loginSchema = z.object({
+    username: z.string({message: 'Username is required '}),
+    password: z.string({message: 'Password is required'}).min(8, {message: 'Password must be least 8 characters'})
 })
